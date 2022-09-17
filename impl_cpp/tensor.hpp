7ad1b3aabf8e61f85bf2515b00c63ae804ecf2a4
fyra <fyra@foxmail.com>
2022-09-18 02:25:09 +0800

    实现了dimension的部分功能

--- impl_cpp/tensor.hpp
+++ impl_cpp/tensor.hpp
@@ -11,17 +11,72 @@
 #ifndef __TENSOR_H__
 #define __TENSOR_H__
 
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
 class dimension {
     public:
-        dimension(int num,  int *arr);
+        dimension(string str);
+        int& operator[](int i);
+        int length();
     private:
+        dimension(int num,  int *arr);
         int num;
         int *arr;
 };
 
-dimension::dimension(int num,  int *arr){
+/**
+ * @brief Construct a new dimension::dimension object
+ * 
+ * @param num 有几个维度
+ * @param arr 各个维度的大小（数组）
+ */
+dimension::dimension(int num,  int *arr)
+{
     this->num = num;
     this->arr = arr;
+}
+
+/**
+ * @brief Construct a new dimension::dimension object
+ * 
+ * @param str 字符串输入  形式: "3,1,2,3" 
+                第一个数字代表之后有几个值
+ */
+dimension::dimension(string str)
+{
+    int *arr;
+    int num;
+
+    const char *s = str.c_str();
+    printf("dimension %s\n", s);
+    sscanf(s, "%d", &num);
+    s = strchr(s, ',')+1;
+    arr = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        sscanf(s, "%d", &arr[i]);
+        s = strchr(s, ',')+1;
+    }
+    this->num = num;
+    this->arr = arr;
+}
+
+int& dimension::operator[](int i)
+{
+    if (i > num) 
+    {// 超出索引
+        return arr[0];
+    }
+    return arr[i];
+}
+
+int dimension::length(){
+    return num;
 }
 
 class tensor{
--- impl_cpp/test.cpp
+++ impl_cpp/test.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "tensor.hpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     cout << "hello wolrd" << endl;
+    dimension d = dimension("3,3,125,125");
+    cout << d.length() << endl;
+    for (int i = 0; i < d.length(); i++)
+    {
+        cout << "d[i]="<<d[i] << endl;
+    }
+
     return 0;
 }
