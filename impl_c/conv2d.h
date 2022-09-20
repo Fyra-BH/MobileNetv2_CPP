72f072488b9cc1e659cff25c7302e6f4d567e946
huangs <huangs@pcl.ac.cn>
2022-09-20 15:31:09 +0800

    新增两种卷积

--- impl_c/conv2d.h
+++ impl_c/conv2d.h
@@ -4,5 +4,7 @@
 #include "dtype.h"
 
 void conv2d_s1p1k3(featureMap *img_in, kernel *ker, featureMap *img_out);
+void conv2d_pw(featureMap *img_in, kernel *ker, featureMap *img_out);
+void conv2d_s1p1k3_dw(featureMap *img_in, kernel *ker, featureMap *img_out);
 
 #endif
