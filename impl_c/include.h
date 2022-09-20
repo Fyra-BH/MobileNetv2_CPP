f026f095e6d904b3812c1dded7bb696cd3369e31
huangs <huangs@pcl.ac.cn>
2022-09-20 12:42:27 +0800

    添加宏函数 用于创建空特征图

--- impl_c/include.h
+++ impl_c/include.h
@@ -5,6 +5,12 @@
 
 #define DEBUG 1
 
+// 头文件汇总
+#include "conv2d.h"
+#include "dataLoadSave.h"
+
+
+// 函数宏
 #ifdef EMBEDDED
     // TODO: 适用于单片机的动态内存分配
 #else
@@ -14,8 +20,14 @@
 
 #define debug(...) {if (DEBUG)printf(__VA_ARGS__);}
 
-// 头文件汇总
-#include "conv2d.h"
-#include "dataLoadSave.h"
+#define emptyFeatureMap(img, c, w, h) ({\
+    img.channels = c;\
+    img.width = w;\
+    img.height = h;\
+    img.data = (float *)MEM_ALLOC(c*w*h * sizeof(float));\
+    for (int i = 0; i < c*w*h; i++)\
+        img.data[i] = 0.0;\
+    c*w*h;\
+})
 
 #endif
--- test/impl_c/conv2d_test.c
+++ test/impl_c/conv2d_test.c
@@ -8,21 +8,11 @@ int main(int argc, char const *argv[])
     load_feature_map_from_file(&img_out_py, "out.txt");
     load_kernel_from_file(&ker, "ker.txt");
 
-    img_out_c.channels = img_out_py.channels;
-    img_out_c.width = img_out_py.width;
-    img_out_c.height = img_out_py.height;
-    int len = img_out_c.width * img_out_c.height * img_out_c.channels;
-
-    img_out_c.data = (float *)MEM_ALLOC(len * sizeof(float));
-    debug("len= %d\n", len);
-    for (int i = 0; i < len; i++)
-    {
-        img_out_c.data[i] = 0.0;
-    }
-    
+    int len = emptyFeatureMap(img_out_c, img_out_py.channels,\
+        img_out_py.width, \
+        img_out_py.height);
+        
     conv2d_s1p1k3(&img_in, &ker, &img_out_c);
-    save_feature_map_to_file(img_in, "imgc.txt");
-    save_kernel_to_file(ker, "kerc.txt");
     save_feature_map_to_file(img_out_c, "outc.txt");
 
     float err = 0.0;
